c213e68b996462e8fef128856be1c795a26121d5
sherifkandeel <sherifkandel4@gmail.com>
2014-10-15 03:48:30 +0200

    adding feature methods for - no of apps made by the developer - Date published - operating system - No of downloads

--- InputDataFormatter.py
+++ InputDataFormatter.py
@@ -29,11 +29,16 @@ import re
 # sample_high_maturity_input = '{"price": "0", "developer": "\u0643\u062a\u0628 \u0627\u0633\u0644\u0627\u0645\u064a\u0629", "reviewers": "643", "title": "\u062a\u0639\u0644\u064a\u0645 \u0627\u0644\u0635\u0644\u0627\u0629 \u0644\u0644\u0627\u0637\u0641\u0627\u0644", "dev_website": "https://www.google.com/url?q=http://codendot.com&sa=D&usg=AFQjCNEm3ZFyZ-5aEURUMzGphrVawo_ICw", "email": "islamicbooks12@gmail.com", "content_rating": "High Maturity", "date_published": "October 18, 2013", "operating_system": "2.2 and up", "downloads": "100,000 - 500,000", "category": "Education", "developer_link": "/store/apps/developer?id=%D9%83%D8%AA%D8%A8+%D8%A7%D8%B3%D9%84%D8%A7%D9%85%D9%8A%D8%A9", "app_url": "https://play.google.com/store/apps/details?id=com.codendot.salatTutorialar", "Description": " \u0625\u0646 \u0633\u062a\u0648\u0646 \u0641\u064a \u0627\u0644\u0645\u0627\u0626\u0629 \u0645\u0646 \u0627\u0644\u0623\u0637\u0641\u0627\u0644 \u0627\u0644\u0645\u0633\u0644\u0645\u064a\u0646 \u0627\u0644\u0630\u064a\u0646 \u064a\u0639\u064a\u0634\u0648\u0646 \u0641\u064a \u0627\u0644\u0645\u062c\u062a\u0645\u0627\u0639\u0627\u062a \u0627\u0644\u063a\u0631\u0628\u064a\u0629 \u0644\u0627 \u064a\u0639\u0644\u0645\u0648\u0646 \u0643\u064a\u0641\u064a\u0629 \u0627\u0644\u0635\u0644\u0627\u0629. \u0645\u0647\u0645\u062a\u0646\u0627 \u0647\u064a \u0625\u0633\u062a\u062d\u062f\u062b \u062a\u0637\u0628\u064a\u0642\u0627\u062a \u0645\u0641\u064a\u062f\u0629 \u0644\u0644\u0637\u0641\u0644 \u0627\u0644\u0645\u0633\u0644\u0645 \u0627\u0644\u0630\u064a \u0647\u0648 \u0628\u062d\u0627\u062c\u0629 \u0644\u0645\u0639\u0631\u0641\u0629 \u0623\u0643\u062b\u0631 \u0639\u0646 \u062f\u064a\u0646 \u0627\u0644\u0625\u0633\u0644\u0627\u0645 \u0648\u062a\u0639\u0627\u0644\u064a\u0645\u0647 \u0648\u0643\u064a\u0641\u064a\u0629 \u0645\u0645\u0627\u0631\u0633\u0629 \u0627\u0644\u0641\u0631\u0627\u0626\u0636. \u0625\u0646 \u062a\u0637\u0628\u064a\u0642 \"\u062a\u0639\u0644\u064a\u0645 \u0627\u0644\u0635\u0644\u0627\u0629 \" \u0647\u062f\u0641\u0647\u0627 \u062a\u0639\u0644\u064a\u0645 \u0627\u0644\u0623\u0637\u0641\u0627\u0644 \u0643\u0644 \u0634\u064a\u0621 \u0639\u0646 \u0627\u0644\u0635\u0644\u0627\u0629 \u0625\u0630 \u064a\u0634\u0631\u062d \u0639\u0646 \u0645\u0648\u0627\u0642\u064a\u062a \u0627\u0644\u0635\u0644\u0627\u0629 \u0643\u0645\u0627 \u0643\u064a\u0641\u064a\u0629 \u0627\u0644\u0635\u0644\u0627\u0629. \u0645\u0646 \u062e\u0644\u0627 \u0647\u0630\u0627 \u0627\u0644\u062a\u0637\u0628\u064a\u0642 \u064a\u062a\u0639\u0644\u0645 \u0627\u0644\u0637\u0641\u0644 \u0645\u062c\u0645\u0648\u0639\u0629 \u0645\u0641\u0631\u062f\u0627\u062a \u0648\u0645\u0635\u0637\u0644\u062d\u0627\u062a \u0645\u062b\u0644: \u0631\u0643\u0648\u0639\u060c \u0633\u062c\u0648\u062f\u060c \u0648\u0636\u0648\u0621\u060c \u0635\u0644\u0627\u0629 \u0627\u0644\u0641\u062c\u0631\u060c \u0635\u0644\u0627\u0629 \u0627\u0644\u0638\u0647\u0631\u060c \u0635\u0644\u0627\u0629 \u0627\u0644\u0639\u0634\u064a. \u0623\u0637\u0644\u0642\u0646\u0627 \u0627\u0644\u0622\u0646 \u062a\u0637\u0628\u064a\u0642 \"\u062a\u0639\u0644\u064a\u0645 \u0627\u0644\u0635\u0644\u0627\u0629 \u0644\u0644\u0623\u0637\u0641\u0627\u0644\" \u0648\u0647\u0648 \u0623\u0648\u0644 \u062a\u0637\u0628\u064a\u0642 \u0645\u0646 \u0633\u0644\u0633\u0629 \u0646\u0633\u0639\u0649 \u0627\u0644\u0649 \u062a\u0637\u0648\u064a\u0631\u0647\u0627 \u0648\u0625\u062e\u0631\u0627\u062c\u0647\u0627 \u0644\u0644\u0645\u0633\u062a\u062e\u062f\u0645\u064a\u0646 \u0648\u0647\u064a: \u2022\u062a\u0639\u0644\u0628\u0645 \u0627\u0644\u0635\u0644\u0627\u0629 \u0628\u0644\u063a\u0627\u062a \u0623\u062c\u0646\u0628\u064a\u0629 \u0645\u062b\u0644 \u0627\u0644\u0641\u0631\u0646\u0633\u064a\u0629\u060c \u0627\u0644\u0625\u0646\u0643\u0644\u064a\u0632\u064a\u0629\u060c \u0627\u0644\u0628\u0631\u062a\u063a\u0627\u0644\u064a\u0629 \u0648\u063a\u064a\u0631\u0647\u0627... \u2022\u062a\u0639\u0644\u064a\u0645 \u0627\u0644\u0648\u0636\u0648\u0621 \u2022\u062a\u0639\u0644\u064a\u0645 \u0627\u0644\u0635\u064a\u0627\u0645 \u2022\u062a\u0639\u0644\u064a\u0645 \u0641\u0631\u0627\u0626\u0636 \u0627\u0644\u062d\u062c \u0644\u0625\u0646\u062c\u0627\u0632 \u0647\u0630\u0647 \u0627\u0644\u062a\u0637\u0628\u064a\u0642\u0627\u062a \u0644\u064a\u0632\u0645\u0646\u0627 \u062f\u0639\u0645\u0627\u064b \u0645\u0627\u062f\u064a\u0627\u064b \u0644\u064a\u0635\u0627\u0631 \u0627\u0644\u0649 \u062a\u063a\u0637\u064a\u0629 \u0627\u0644\u0645\u0635\u0627\u0631\u064a\u0641 \u0627\u0644\u0645\u062a\u0631\u062a\u0628\u0629\u060c \u0648\u0645\u0646 \u0647\u0646\u0627 \u0644\u0645\u0633\u0647\u0627\u0645\u062a\u0643\u0645 \u062f\u0648\u0631\u0627\u064b \u0643\u0628\u064a\u0631\u0627\u064b \u0641\u064a \u062a\u062d\u0642\u064a\u0642 \u063a\u0627\u064a\u062a\u0646\u0627. \u0641\u0631\u064a\u0642 \u0627\u0644\u0639\u0645\u0644: \u0646\u062d\u0646 \u0645\u062c\u0645\u0648\u0639\u0629 \u0645\u0646 \u0645\u0637\u0648\u0631\u064a \u0627\u0644\u0628\u0631\u0627\u0645\u062c \u0625\u0633\u062a\u062d\u062f\u062b\u0646\u0627 \u062a\u0637\u0628\u064a\u0642 \u0645\u0641\u064a\u062f \u0644\u0644\u0637\u0641\u0644 \u0627\u0644\u0645\u0633\u0644\u0645 \u0627\u0644\u0630\u064a \u0647\u0648 \u0628\u062d\u0627\u062c\u0629 \u0644\u0645\u0639\u0631\u0641\u0629 \u0623\u0643\u062b\u0631 \u0639\u0646 \u062f\u064a\u0646 \u0627\u0644\u0625\u0633\u0644\u0627\u0645 \u0648\u062a\u0639\u0627\u0644\u064a\u0645\u0647 \u0648\u0643\u064a\u0641\u064a\u0629 \u0645\u0645\u0627\u0631\u0633\u0629 \u0627\u0644\u0641\u0631\u0627\u0626\u0636. \u0646\u062d\u0646 \u0642\u0633\u0645 \u0645\u0646 \u0634\u0631\u0643\u0629 \u0643\u0648\u062f \u0623\u0646\u062f \u062f\u0648\u062a www.codendot.com \u0647\u062f\u0641\u0646\u0627 \u062a\u062b\u0642\u064a\u0641 \u0645\u062c\u062a\u0645\u0639\u0646\u0627 \u0648\u062a\u0648\u0639\u064a\u062a\u0647 \u0648\u0646\u0639\u0645\u0644 \u062a\u062d\u062a \u0625\u0633\u0645 Islamic Books Sixty per cent of Muslim children living in Western communities who do not know how to pray. Our mission is developed useful applications for the Muslim child who is a need to learn more about the religion of Islam and its teachings and how to practice statutes.The application of the \"Prayer education\" aimed at teaching children all about prayer as explain about prayer times as how to pray.During this application the child learns a vocabulary of terms such as: bowing, prostration, and the light, the dawn prayer, noon prayer, Ashi prayer.We now apply \"prayer, education for children,\" which is the first application of a series seeking to develop and removing users, namely:\u2022 Talpm prayer in foreign languages \u200b\u200bsuch as French, English, Portuguese and other ...\u2022 Education ablution\u2022 Education fasting\u2022 Education ordinances pilgrimageTo accomplish these applications to Esmna support financially so they can be to cover the expenses incurred, hence Meshamtkm a major role in achieving our goal.Team:We are a group of software developers we have introduced a useful application for the Muslim child who is a need to learn more about the religion of Islam and its teachings and how to practice statutes.We are a division of On & Dot www.codendot.com our community education and awareness and working under the name of Islamic Books  ", "rating": "4.1493000984191895"}'
 # sample_string_with_quote_error = '{"price": "0", "developer": "\"MMH Dev\"", "reviewers": "8270", "title": "English Arabic Dictionary", "dev_website": "https://www.google.com/url?q=http://www.iquest-es.com&sa=D&usg=AFQjCNG8o0hL6sk-VOmUJ_JJdBrlR94k_w", "email": "mmh@iquest-es.com", "content_rating": "Everyone", "date_published": "September 8, 2014", "operating_system": "2.3.3 and up", "downloads": "500,000 - 1,000,000", "category": "Education", "developer_link": "/store/apps/developer?id=MMH+Dev", "app_url": "https://play.google.com/store/apps/details?id=com.mmh.qdic", "Description": " Q DictionaryFree offline Arabic-English and English-Arabic Dictionary, with simple, beautiful and easy to use interface.First dictionary to support translation from other applications in a very simple way.Q Dictionary Features:- English Arabic and Arabic English translation- Used offline- Cross apps translation to translated from other apps like web browser, mail or sms without opining Q Dictionary and without interruptions- \u0650Automatic detection of word root- 87k+ English words, 94k+ Arabic words including a lot of abbreviations- Automatic language detection- Fast search with automatic suggestions- Speech-To-Text- English words Text-To-Speech- Word search history- Favorite words list- Copy or share translation- Search for words on Google, Wikipedia or Wiktionary  ", "rating": "4.188028812408447"}'
 # sample_non_english_developer = sample_high_maturity_input
+import MySQLdb
+from datetime import datetime
+from datetime import  date
 
 
 class InputDataFormatter(object):
     def __init__(self, json_object):
         self.source_object = json_object
+        # self.developers = self.fetch_developer_data()
+        self.android_versions_dict = self.load_android_versions()
 
     def string(self):
         return json.dumps(self.source_object)
@@ -66,6 +71,41 @@ class InputDataFormatter(object):
             return 3
 
         return -1
+
+    def fetch_developer_data(self):
+        self.db = MySQLdb.connect("localhost", "root", "root", "appsdb", use_unicode=True, charset="utf8")
+        sql = "SELECT distinct (developer), count(*) as count FROM appsdb.apps_default Group by developer"
+        cur = self.db.cursor()
+        cur.execute(sql)
+        rows = dict(cur.fetchall())
+        return rows
+
+    def number_of_apps_for_developer(self):
+        return self.developers[self.source_object["developer"]]
+
+    def date_published_difference(self):
+        date_string = self.source_object["date_published"]
+        publish_date = datetime.strptime(date_string, '%B %d, %Y').date()
+        refernce_date = date(2000, 1, 1)
+        difference = publish_date - refernce_date
+        return difference.days
+
+    def load_android_versions(self):
+        dict = {}
+        print (type(dict))
+        counter = 0
+        with open('android_versions_enum') as f:
+            for line in f:
+                dict[line.rstrip()] = counter
+                counter+=1
+        return dict
+
+    def android_version_enum(self):
+        return self.android_versions_dict[self.source_object["operating_system"]]
+
+    def number_of_downloads(self):
+        return len(self.source_object["downloads"])
+
 
 
 class InputDataFormatterTest(TestCase):
@@ -124,5 +164,28 @@ class InputDataFormatterTest(TestCase):
 
         self.formatter = InputDataFormatter(self.sample_high_maturity_input)
         self.assertEqual(self.formatter.content_rating(), 3)
+
+    def test_fetch_developer_data(self):
+        self.assertEqual(len(self.formatter.fetch_developer_data()), 266340)
+        pass
+
+    def test_number_of_apps_for_developer(self):
+        self.formatter = InputDataFormatter(self.sample_input)
+        self.assertEqual(self.formatter.number_of_apps_for_developer(), 2) #number of apps for "MMH Dev" is 2
+        pass
+
+    def test_date_published_difference(self):
+        #self.assertEqual(self.formatter.date_published_difference(), 1000)
+        self.assertEqual(self.formatter.date_published_difference(), 5364)
+        pass
+
+    def test_load_android_versions(self):
+        self.assertEqual(len(self.formatter.load_android_versions()), 135)
+
+    def test_android_version_enum(self):
+        self.assertEqual(self.formatter.android_version_enum(), 1)
+
+    def test_number_of_downloads(self):
+        self.assertEqual(self.formatter.number_of_downloads(), 19)
 
 
